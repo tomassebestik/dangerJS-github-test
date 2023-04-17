5f06e2707e78df41027f2c259680d76b72bff6ae
Tomas Sebestik <tomas.sebestik@espressif.com>
2023-04-17 10:28:46 +0200

    Process as markdown comment, not message

--- .github/dangerjs/prExplainMergeProcess.ts
+++ .github/dangerjs/prExplainMergeProcess.ts
@@ -2,14 +2,21 @@ declare const danger: any;
 declare const markdown: any;
 declare const message: any;
 
+import * as fs from "fs"
+import * as path from "path"
+
+
 export default function (): void {
 	// Define the path to your Markdown file
-	const filePath = 'mergeProcess.md';
+	// const filePath = 'mergeProcess.md';
+
+	// const filePath = path.join(__dirname, "mergeProcess.md")
 
 	// Read the contents of the Markdown file
-	const fs = require('fs');
-	const markdownContent = fs.readFileSync(filePath, 'utf-8');
+	// const fs = require('fs');
+	const markdownContent = fs.readFileSync('mergeProcess.md', 'utf-8');
 
 	// Display the Markdown content to the user
-	message(markdownContent);
+	markdown('\n***\n');
+	markdown(markdownContent);
 }
