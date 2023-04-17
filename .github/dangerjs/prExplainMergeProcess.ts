f786878a2751d0c27b9e221c35e9c975b5bebd8f
Tomas Sebestik <tomas.sebestik@espressif.com>
2023-04-17 10:48:39 +0200

    Finalize merge process document

--- .github/dangerjs/prExplainMergeProcess.ts
+++ .github/dangerjs/prExplainMergeProcess.ts
@@ -1,22 +1,24 @@
-declare const danger: any;
 declare const markdown: any;
-declare const message: any;
 
-import * as fs from "fs"
-import * as path from "path"
+const mergeProcessDescription: string = `
+#### Pull request review and merge process you can expect
+The main development of \`ESP-IDF\` project is done on Espressif Gitlab (internal). The GitHub repository is only a public mirror of Gitlab.
 
+After you open the Pull Request, there will probably be some discussion in the comments field of the request itself. 
+
+The GitHub sync. bot will automatically creates a Jira ticket with your Pull request in Espressif's internal project management system and assign it to the responsible Espressif engineer.
+
+Once the issue is assigned, the assignee evaluates the contribution, tests the changes to the Pull request in internal system, and reviews it with other Espressif engineers.
+
+Once the Pull Request is ready to merge, it will first be merged into our internal git system (Gitlab) for in-house automated testing.
+
+If this process passes, it will be merged onto the public github repository.
+
+You can read more about this process in [Contributions Guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/contribute/index.html#contributions-guide)
+
+***
+`;
 
 export default function (): void {
-	// Define the path to your Markdown file
-	// const filePath = 'mergeProcess.md';
-
-	// const filePath = path.join(__dirname, "mergeProcess.md")
-
-	// Read the contents of the Markdown file
-	// const fs = require('fs');
-	const markdownContent = fs.readFileSync('mergeProcess.md', 'utf-8');
-
-	// Display the Markdown content to the user
-	markdown('\n***\n');
-	markdown(markdownContent);
+	return markdown(mergeProcessDescription);
 }
