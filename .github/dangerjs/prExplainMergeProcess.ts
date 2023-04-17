c2a0dce1231a893d188b2bf3867d59b876304108
Tomas Sebestik <tomas.sebestik@espressif.com>
2023-04-17 10:16:43 +0200

    Add explaining merge process

--- .github/dangerjs/prExplainMergeProcess.ts
+++ .github/dangerjs/prExplainMergeProcess.ts
@@ -1,1 +1,15 @@
-export default function (): void {}
+declare const danger: any;
+declare const markdown: any;
+declare const message: any;
+
+export default function (): void {
+	// Define the path to your Markdown file
+	const filePath = 'mergeProcess.md';
+
+	// Read the contents of the Markdown file
+	const fs = require('fs');
+	const markdownContent = fs.readFileSync(filePath, 'utf-8');
+
+	// Display the Markdown content to the user
+	message(markdownContent);
+}
